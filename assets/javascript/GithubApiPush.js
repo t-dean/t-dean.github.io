4e8d4783870a20eee869832aeb98e3d5f15738f9
Taqi <ahmadtaqiyuddin98@yahoo.com>
2021-03-02 20:33:08 +0800

    new

---
+++ assets/javascript/GithubApiPush.js
@@ -0,0 +1,166 @@
+'use strict';
+
+//Uses the https://github.com/github-tools/github library under the hood and exposes it as `gh` property
+function GithubAPI(auth) {
+    var repo = void 0;
+    var filesToCommit = [];
+    var currentBranch = {};
+    var newCommit = {};
+
+    //the underlying library for making requests
+    var gh = new GitHub(auth);
+
+    /**
+     * Sets the current repository to make push to
+     * @public
+     * @param {string} userName Name of the user who owns the repository
+     * @param {string} repoName Name of the repository
+     * @return void
+     */
+    this.setRepo = function (userName, repoName) {
+        repo = gh.getRepo(userName, repoName);
+    };
+
+    /**
+     * Sets the current branch to make push to. If the branch doesn't exist yet,
+     * it will be created first
+     * @public
+     * @param {string} branchName The name of the branch
+     * @return {Promise}
+     */
+    this.setBranch = function (branchName) {
+        if (!repo) {
+            throw 'Repository is not initialized';
+        }
+
+        return repo.listBranches().then(function (branches) {
+
+            var branchExists = branches.data.find(function (branch) {
+                return branch.name === branchName;
+            });
+
+            if (!branchExists) {
+                return repo.createBranch('master', branchName).then(function () {
+                    currentBranch.name = branchName;
+                });
+            } else {
+                currentBranch.name = branchName;
+            }
+        });
+    };
+
+    /**
+     * Makes the push to the currently set branch
+     * @public
+     * @param  {string}   message Message of the commit
+     * @param  {object[]} files   Array of objects (with keys 'content' and 'path'),
+     *                            containing data to push
+     * @return {Promise}
+     */
+    this.pushFiles = function (message, files) {
+        if (!repo) {
+            throw 'Repository is not initialized';
+        }
+        if (!currentBranch.hasOwnProperty('name')) {
+            throw 'Branch is not set';
+        }
+
+        return getCurrentCommitSHA().then(getCurrentTreeSHA).then(function () {
+            return createFiles(files);
+        }).then(createTree).then(function () {
+            return createCommit(message);
+        }).then(updateHead).catch(function (e) {
+            console.error(e);
+        });
+    };
+
+    /**
+     * Sets the current commit's SHA
+     * @private
+     * @return {Promise}
+     */
+    function getCurrentCommitSHA() {
+        return repo.getRef('heads/' + currentBranch.name).then(function (ref) {
+            currentBranch.commitSHA = ref.data.object.sha;
+        });
+    }
+
+    /**
+     * Sets the current commit tree's SHA
+     * @private
+     * @return {Promise}
+     */
+    function getCurrentTreeSHA() {
+        return repo.getCommit(currentBranch.commitSHA).then(function (commit) {
+            currentBranch.treeSHA = commit.data.tree.sha;
+        });
+    }
+
+    /**
+     * Creates blobs for all passed files
+     * @private
+     * @param  {object[]} filesInfo Array of objects (with keys 'content' and 'path'),
+     *                              containing data to push
+     * @return {Promise}
+     */
+    function createFiles(filesInfo) {
+        var promises = [];
+        var length = filesInfo.length;
+
+        for (var i = 0; i < length; i++) {
+            promises.push(createFile(filesInfo[i]));
+        }
+
+        return Promise.all(promises);
+    }
+
+    /**
+     * Creates a blob for a single file
+     * @private
+     * @param  {object} fileInfo Array of objects (with keys 'content' and 'path'),
+     *                           containing data to push
+     * @return {Promise}
+     */
+    function createFile(fileInfo) {
+        return repo.createBlob(fileInfo.content).then(function (blob) {
+            filesToCommit.push({
+                sha: blob.data.sha,
+                path: fileInfo.path,
+                mode: '100644',
+                type: 'blob'
+            });
+        });
+    }
+
+    /**
+     * Creates a new tree
+     * @private
+     * @return {Promise}
+     */
+    function createTree() {
+        return repo.createTree(filesToCommit, currentBranch.treeSHA).then(function (tree) {
+            newCommit.treeSHA = tree.data.sha;
+        });
+    }
+
+    /**
+     * Creates a new commit
+     * @private
+     * @param  {string} message A message for the commit
+     * @return {Promise}
+     */
+    function createCommit(message) {
+        return repo.commit(currentBranch.commitSHA, newCommit.treeSHA, message).then(function (commit) {
+            newCommit.sha = commit.data.sha;
+        });
+    }
+
+    /**
+     * Updates the pointer of the current branch to point the newly created commit
+     * @private
+     * @return {Promise}
+     */
+    function updateHead() {
+        return repo.updateHead('heads/' + currentBranch.name, newCommit.sha);
+    }
+};
---
+++ assets/javascript/cms.js
@@ -0,0 +1,139 @@
+
+$(function(){
+    $('#ghsubmitbtn').on('click', function(e){
+        e.preventDefault();
+        var owner = $('#owner').val();
+        var token = $('#ghToken').val();
+        var repo = $('#repo').val();
+        var path = $('#path').val();
+        var schema = $('#schema').val();
+        var alert = $('.alert');
+        var resultsContainer = $('#results');
+        var submitButtonText = "Save Changes";
+        var resetButtonText = "Reset Changes";
+        var hasClicked = false;
+        var didSubmit = false;
+        var dataStore = {
+            data: {},
+            schema: {}
+        };
+
+        if(!hasClicked){
+            $.when(
+                $.ajax({
+                    url: "https://api.github.com/repos/" + owner + "/" + repo + "/contents/" + path,
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("Authorization", "user" + btoa("token:" + token));
+                    },
+                    type: 'GET',
+                    success: function (data) {
+                        dataStore.data = data;
+						console.log('Got some data!', data);
+                    },
+                    error: function (err) {
+                        alert.addClass('error').removeClass('hidden').html('Something went wrong:', err);
+						console.log('can not connect to Github for data', err);
+                    }
+                }),
+                $.ajax({
+                    url: "https://api.github.com/repos/" + owner + "/" + repo + "/contents/" + schema,
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("Authorization", "user" + btoa("token:" + token));
+                    },
+                    type: 'GET',
+                    success: function (schema) {
+                        dataStore.schema = schema;
+						console.log('Got some schemas!', schema);
+                    },
+                    error: function (err) {
+                        alert.addClass('error').removeClass('hidden').html('Something went wrong:', err);
+						console.log('can not connect to Github for schema', err);
+                    }
+                })
+
+            ).then(function() {
+                var projectData = dataStore.data;
+                var jsonFile = projectData.content;
+				var schemaData = dataStore.schema;
+                hasClicked = true;
+                var schemaFile = schemaData.content;
+				var decodedJson = decodeURIComponent(escape(window.atob(jsonFile)));
+				var parsedDecodedJson = JSON.parse(decodeURIComponent(escape(window.atob(decodedJson))));
+				var decodedSchemaJson = JSON.parse(atob(schemaFile));
+
+				hasClicked = true;
+                if(parsedDecodedJson){
+                    $('#login').hide();
+                    alert.addClass('hidden').removeClass('alert');
+
+                    var plugins = JSONEditor.plugins || {};
+                    var sceditor = plugins.sceditor || {};
+                    if (plugins && sceditor) {
+                        sceditor.enable = true;
+                    }
+
+                    var editor = new JSONEditor(document.getElementById('results'),{
+                        ajax: true,
+                        disable_edit_json: true,
+                        schema: decodedSchemaJson,
+                        theme: 'bootstrap3',
+                        startval: parsedDecodedJson,
+                        iconlib: "bootstrap3"
+                    });
+
+                    resultsContainer.append('<button class="submit-btn btn btn-primary">'+submitButtonText+'</button>');
+                    resultsContainer.append('<button class="reset-btn btn btn-primary">'+resetButtonText+'</button>');
+
+                    $('.submit-btn').on('click', function( e ) {
+                        e.preventDefault();
+                        if(!didSubmit){
+                            var owner = $('#owner').val();
+                            var token = $('#ghToken').val();
+                            var repo = $('#repo').val();
+                            var path = $('#path').val();
+                            var branch = $('#branch').val();
+                            var formData = editor.getValue();
+                            var encodedJsonData = btoa(unescape(encodeURIComponent(JSON.stringify(formData))));
+
+                            didSubmit = true;
+                            $(this).addClass('disabled');
+
+                            var api = new GithubAPI({ token: token});
+                            api.setRepo(owner, repo);
+                            api.setBranch(branch).then(function () {
+                                return api.pushFiles(
+                                    'CMS Update',
+                                    [
+                                        {
+                                            content: encodedJsonData,
+                                            path: path
+                                        }
+                                    ]
+                                );
+                            }).then(function () {
+                                console.log('Files committed!');
+                                alert.addClass('success').removeClass('hidden').html('Files succesfully submitted to Github.');
+                                $('.submit-btn').removeClass('disabled');
+                                setTimeout(function(){
+                                    alert.addClass('hidden').removeClass('success');
+                                },4000);
+                                didSubmit = false;
+                            }).catch(function(err){
+                                alert.addClass('error').removeClass('hidden').html('Something went wrong:', err);
+								console.log('Commit error!', err);
+                            });
+                        }
+                    });
+
+                    $('.reset-btn').on('click', function(e) {
+                        e.preventDefault();
+                        editor.setValue(parsedDecodedJson);
+                    });
+                } else {
+                    alert.addClass('error').removeClass('hidden').html('Something is wrong with the JSON. Make sure it is valid');
+					console.log('Bad JSON!');
+				}
+            });
+        }
+    });
+});
